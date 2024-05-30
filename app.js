6cfd287a89ba38408a15aba809d0fba524508053
ShahzaibKhan <szaibkhan4@gmail.com>
2024-05-30 20:21:58 +0500

    complete quiz app getting data from api

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const body = document.getElementsByTagName('body')[0]; // Select the first <body> element
 let question = document.getElementById("question");
+let btn = document.getElementById("submit");
+
+let index = 0; 
+let correct = 0; 
+let wrong = 0; 
 
 let box = document.getElementsByClassName("box")[0]; // Select the first element with class "box"
 var quizData;
 
 function getQuizData() {
-    fetch("https://opentdb.com/api.php?amount=10&category=23")
+    fetch("https://opentdb.com/api.php?amount=5&category=23")
         .then((response) => response.json())
         .then((data) => {
             quizData = data.results;
@@ -14,30 +19,58 @@ function getQuizData() {
         })
         .catch(() => {
             alert("Something went wrong, please refresh.");
-        });
+        }); 
 }
 
 function populateBox(quizData) {
     let optionsetBox = document.getElementById('optionsetBox');
 
     let optionsets = '';
-    question.innerText = quizData[0].question;
-
-    let incorrect_answers = quizData[0].incorrect_answers;
-
+    
+    let incorrect_answers = quizData[index].incorrect_answers;
+    
     let randomNumber = Math.floor(Math.random() * (incorrect_answers.length + 1));
     
-    incorrect_answers.splice(randomNumber, 0, quizData[0].correct_answer);
+    question.innerText = quizData[index].question;
+    incorrect_answers.splice(randomNumber, 0, quizData[index].correct_answer);
 
-    incorrect_answers.forEach((e) => {
+    incorrect_answers.forEach((e, i) => {
         optionsets += `<div class="row">
-                <input name="choice" type="radio">
-                <label for="choice">${e}</label>
+                <input id="choice${i}" name="choice" type="radio">
+                <label for="choice${i}">${e}</label>
             </div>`;
     });
 
     optionsetBox.innerHTML = optionsets;
 }
+
+btn.addEventListener('click', function onClick(e) {
+    e.preventDefault();
+    
+    let selectedOption = document.querySelector('input[name="choice"]:checked');
+    
+    if (selectedOption) {
+        let labelValue  = selectedOption.nextElementSibling.innerText;
+        console.log(labelValue);
+
+        if (quizData[index].correct_answer == labelValue) {
+            correct++;
+            console.log(correct);
+        } else {
+            wrong++;
+            console.log(wrong);
+        }
+
+        index++;
+        if (index < quizData.length) {
+            populateBox(quizData);
+        } else {
+           // alert(`Quiz finished! Correct: ${correct}, Wrong: ${wrong}`);
+            box.innerHTML = `<h2 id="finish">  Quiz finished! <br>Correct: ${correct}, Wrong: ${wrong}</h2>`;        }
+    } else {
+        alert("No option selected");
+    }
+});
 
 // Function call
 getQuizData();
